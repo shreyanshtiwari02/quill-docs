0908af4ebc33e4f50daf64f67bec8706ef08d296
Shreyansh <shreyanshtiwari2002excel@gmail.com>
2023-09-27 21:47:24 +0530

    text-editor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import TextEditor from "./TextEditor"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom"
+import { v4 as uuidV4 } from "uuid"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+        <Routes>
+          <Route path="/" element={<a href={`/documents/${uuidV4()}`}>create document</a>} />
+          <Route path='/documents/:id' element={ <TextEditor />}/>
+        </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
